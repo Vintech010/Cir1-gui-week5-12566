54a867ab3e9280287056dd6e50e456af7557c441
Vintech010 <azakisakuya@gmail.com>
2023-06-13 13:02:15 +0700

    operator

---
+++ bin/dart_week5_dart.dart
@@ -0,0 +1,11 @@
+void main (){
+  print("Assignment operator");
+  print("============");
+  const int n1 = 10;
+  const int n2 = 5;
+  print("$n1 + $n2 = ${n1 + n2}");//ADD บวก/เพิ่ม
+  print("$n1 - $n2 = ${n1 - n2}");//Substraction ลบ
+  print("$n1 * $n2 = ${n1 * n2}");//Multication คูณ
+  print("$n1 / $n2 = ${n1 / n2}");//Division หาร
+  print("$n1 % $n2 = ${n1 % n2}");//Modulus หาเศษ
+}
---
+++ test/dart_week5_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_week5_dart/dart_week5_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
