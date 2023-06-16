7596540e8f8a27fe3be511055fd9135862eb285a
Vintech010 <azakisakuya@gmail.com>
2023-06-16 09:31:06 +0700

    update

---
+++ bin/unary_operator.dart
@@ -0,0 +1,8 @@
+void main(){
+  print("Dart unary operators:");
+  int x = 20;
+  int y = 10;
+  // String y = '10';
+  // variable y : Is string and can be used but not int Cuz it have '10' 10 = char
+  print("x-y = ? | $x-$y = ${x - y}"); //Subtraction : 20-10 = 10
+}
